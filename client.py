776a50e2cf7f9acd5db8a09a1f216db0cd98458d
ad-chaos <santoshrajpurohit89@gmail.com>
2024-03-03 16:55:08 +0530

    add a simple commandline interface

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
-import sys
 from packet_parser import VOIP_PORT, PacketType, Packet
 from libclient import Client
 from typing import Iterator
+import argparse
 
 
 class VoIPClient(Client):
@@ -10,29 +10,44 @@ class VoIPClient(Client):
             print(pkt.msg.extra)
 
 
-def main(
-    username: str, password: str, reciever: str, producer: Iterator[Packet]
-) -> None:
-    with VoIPClient(username, password, reciever, ("localhost", VOIP_PORT)) as c:
-        status = c.login()
-        if status is None or status.ty == PacketType.InvalidUser:
-            return
-        c.wait_for_reciever()
-        print(f"\nPairing successful with {c.reciever}!")
-        c.chat(producer)
-
-
-if __name__ == "__main__":
-    if sys.argv[1] == "a":
-        print("I'm ad")
-        username = "ad"
-        password = "a"
-        reciever = "sad"
-    else:
-        print("I'm sad")
-        username = "sad"
-        password = "s"
-        reciever = "ad"
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        prog="VoIP",
+        description="A client for talking to a VoIP server",
+        epilog="Made with ❤ from Kiran & Karthik",
+    )
+    parser.add_argument(
+        "-u", "--username", required=True, help="Username you want to login as"
+    )
+    parser.add_argument(
+        "-p", "--password", required=True, help="Password for the given username"
+    )
+    parser.add_argument(
+        "-r",
+        "--reciever",
+        required=True,
+        help="The name of reciever you want to talk to",
+    )
+    parser.add_argument(
+        "-i", "--ip", default="localhost", help="The ip address of the server"
+    )
+    grp = parser.add_mutually_exclusive_group(required=True)
+    grp.add_argument(
+        "-l",
+        "--login",
+        action=argparse.BooleanOptionalAction,
+        default=False,
+        help="Use the credentials for login",
+    )
+    grp.add_argument(
+        "-s",
+        "--sigin",
+        type=bool,
+        action=argparse.BooleanOptionalAction,
+        default=False,
+        help="Use the credentials for sigining in as a first time user",
+    )
+    args = parser.parse_args()
 
     producer: Iterator[Packet] = map(
         Packet.message,
@@ -46,4 +61,19 @@ if __name__ == "__main__":
         * 5,
     )
 
-    main(username, password, reciever, producer)
+    with VoIPClient(
+        args.username, args.password, args.reciever, (args.ip, VOIP_PORT)
+    ) as c:
+        print("Login: ", args.login)
+        print("Sigin: ", args.sigin)
+        status = c.login() if args.login else c.signin()
+        if status is None or status.ty == PacketType.InvalidUser:
+            print(status)
+            return
+        c.wait_for_reciever()
+        print(f"\nPairing successful with {c.reciever}!")
+        c.chat(producer)
+
+
+if __name__ == "__main__":
+    main()
